6e7804da7fe840271a0f841bcb55c91d50b3d64c
kasim3120 <74439980+kasim3120@users.noreply.github.com>
2021-04-08 14:16:21 +0800

    Update jQuery作業.js

--- javascript/jQuery作業.js
+++ javascript/jQuery作業.js
@@ -28,26 +28,63 @@ $(document).ready(function() {
       'positionFromTop': 200,
     })
 
+
 //下拉選單
 
-$('.products').click(function(event) {
-  /* Act on the event */
+// $('.dropdownBtn').click(function(event) {
+//   /* Act on the event */
+//   event.preventDefault();
+//   $('.dropdownBtn').toggleClass('active');
+//   $('.dropdownList').slideToggle();
+// });
+// $('.dropdownList').click(function(event) {
+//   /* Act on the event */
+//   event.preventDefault();
+//   $('.dropdownBtn').toggleClass('active');
+//   $('.dropdownList').slideToggle();
+// });
+
+// Dropdown
+$(".dropdownBtn").click(function (event) {
   event.preventDefault();
-  $('.products').toggleClass('active');
-  $('.productsOpen').slideToggle();
+  event.stopPropagation();
+  $(this).parent().find(".dropdownList").stop().slideToggle();//$(他的).父層().尋找(下拉選單).停止.收合開關
+  $(this).parent().siblings().find(".dropdownList").slideUp();//$(他的).父層().同層().尋找(下拉選單).收合
 });
-$('.liaise').click(function(event) {
-  /* Act on the event */
-  event.preventDefault();
-  $('.liaise').toggleClass('active');
-  $('.liaiseOpen').slideToggle();
+$("html:not(.dropdownBtn)").click(function () {
+  $(".dropdownList").slideUp("slow");//點擊別個dropdownBtn收合(緩慢)另一個dropdownList
 });
 
+
 //回到頂端
-    $('.top').click(function(event) {
-      event.preventDefault();
-      $('html,body').animate({
-        scrollTop: 0
-      }, 1000);
-    });     
+//     $('.gotop').click(function(event) {
+//       event.preventDefault();
+//       $('html,body').animate({
+//         scrollTop: 0 }, 1000);
+//     function showBtnCondition() {
+//       if ($('.gotop').scrollTop() > 600) {
+//         $('gotop').fadeIn();
+//       } else {
+//         $('gotop').fadeOut();
+//       }
+//     }
+//     $(window).scroll(showBtnCondition);
+//     });     
+// });
+
+$(".jq-goTop").click(function (e) {
+  e.preventDefault();
+  $("html,body").animate(
+    {scrollTop: 0,},200 )
+  });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 1200) {
+      if ($(".goTop").hasClass("hide")) {
+        $(".goTop").toggleClass("hide");
+      }
+    } else {
+      $(".goTop").addClass("hide");
+    }
+  });
 });
+
